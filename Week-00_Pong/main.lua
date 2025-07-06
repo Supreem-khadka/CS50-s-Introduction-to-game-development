1d843e565b6cd1ccbecbd915ba8cce8fcf61e4f5
zororo <zororo>
2025-07-06 10:56:23 +0545

    Created main.lua

---
+++ Week-00_Pong/main.lua
@@ -0,0 +1,26 @@
+--[[
+    Author -> Supreem Khadka
+    Implemetation of Week 00 of CS50's Introduction to game development
+--]]
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.draw()
+    love.graphics.printf(
+        "Pong Week 00",     -- text to print
+        0,
+        WINDOW_HEIGHT / 2,
+        WINDOW_WIDTH,
+        "center"
+    )
+end
