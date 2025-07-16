a69d587fda34dcac72d6247ffefa6f8fc88e8498
zororo <zororo>
2025-07-16 10:47:54 +0545

    Score and Countdown Update

---
+++ Week-01_FlappyBird/flappy6/states/CountdownState.lua
@@ -0,0 +1,25 @@
+CountdownState = Class{__includes = BaseState}
+
+local timer  = 0
+
+function CountdownState:init()
+    self.time = 3
+end
+
+
+function CountdownState:update(dt)
+    timer = timer + dt
+    if timer >= 1 then
+        self.time = self.time - 1
+        timer = 0
+    end
+
+    if self.time < 0 then
+        gStateMachine:change("play")
+    end
+end
+
+function CountdownState:render()
+    love.graphics.setFont(hugeFont)
+    love.graphics.printf(tostring(self.time), 0, 100, VIRTUAL_WIDTH, "center")
+end
